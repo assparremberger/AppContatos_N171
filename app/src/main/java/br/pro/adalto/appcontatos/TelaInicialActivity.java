a642ad1c48ef74dc320a20789c44e9ff3e470bd4
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2018-09-17 21:59:35 -0300

    Criação do projeto com as telas TelaInicial e Formulário

---
+++ app/src/main/java/br/pro/adalto/appcontatos/TelaInicialActivity.java
@@ -0,0 +1,13 @@
+package br.pro.adalto.appcontatos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class TelaInicialActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_tela_inicial);
+    }
+}
