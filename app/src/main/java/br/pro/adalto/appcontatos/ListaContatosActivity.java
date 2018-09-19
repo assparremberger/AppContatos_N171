d695a75a70752972b4b81225d2302b4769f2f788
Adalto Selau Sparremberger <assparremberger@senacrs.com.br>
2018-09-18 21:53:45 -0300

    Telas implementadas, botões da tela inicial abrem as outras telas e arquivos de strings e styles implementados

---
+++ app/src/main/java/br/pro/adalto/appcontatos/ListaContatosActivity.java
@@ -0,0 +1,13 @@
+package br.pro.adalto.appcontatos;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ListaContatosActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_lista_contatos);
+    }
+}
--- app/src/main/java/br/pro/adalto/appcontatos/TelaInicialActivity.java
+++ app/src/main/java/br/pro/adalto/appcontatos/TelaInicialActivity.java
@@ -1,13 +1,42 @@
 package br.pro.adalto.appcontatos;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class TelaInicialActivity extends AppCompatActivity {
+
+    private Button btnCadastrar, btnListar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tela_inicial);
+
+        btnCadastrar = (Button) findViewById(R.id.btnCadastrar);
+        btnListar = (Button) findViewById(R.id.btnListar);
+
+        btnCadastrar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(TelaInicialActivity.this,
+                        FormularioActivity.class);
+                startActivity( i );
+            }
+        });
+
+
+        btnListar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent i = new Intent(TelaInicialActivity.this,
+                        ListaContatosActivity.class);
+                startActivity( i );
+            }
+        });
+
     }
+
 }
